b9c57e41a7124d7daaaf8c2a62010ccb3283ec29
ZhiyongWangWzy <91057063+ZhiyongWangWzy@users.noreply.github.com>
2022-11-24 17:31:59 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-python3 test.py --in_folder input_data/syn_L_50 --out_folder out_debug --poolSize 50 --suparmpoolSize 1000 --seedIndex 0 
+python3 test.py --in_folder input_data/syn_L_50 --out_folder out_debug --poolSize 50 --seedIndex 0 
 
 
